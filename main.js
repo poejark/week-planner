41292f89fd091838b4ccf41558250dff8ea58b9f
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-28 19:30:26 +0000

    styling

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var $dialog = document.querySelector('dialog');
-if (!$dialog) throw new Error('dialog element not found.');
+if (!$dialog)
+    throw new Error('dialog element not found.');
 var $addButton = document.querySelector('.new-event');
-if (!$addButton) throw new Error('add new entry button not found.');
+if (!$addButton)
+    throw new Error('add new entry button not found.');
 $addButton.addEventListener('click', function () {
-  $dialog.showModal();
+    $dialog.showModal();
 });
 var $cancelModal = document.querySelector('.cancel-modal');
-if (!$cancelModal) throw new Error('cancel button not found within modal.');
+if (!$cancelModal)
+    throw new Error('cancel button not found within modal.');
 $cancelModal.addEventListener('click', function () {
-  $dialog.close();
+    $dialog.close();
 });
 var $confirmModal = document.querySelector('.confirm-modal');
 $confirmModal.addEventListener('click', function (event) {
-  event.preventDefault();
-  console.log('inside the confirm Modal.');
+    event.preventDefault();
+    console.log('inside the confirm Modal.');
 });
 // const $photoInput = document.querySelector('input[name="photo"]');
