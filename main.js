3857b3ddd3de00702de1de379e74a9f24143458a
poejark <josephpark842@gmail.com>
2025-02-28 18:47:51 +0000

    iteration 3

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-const $dialog = document.querySelector('dialog');
+var $dialog = document.querySelector('dialog');
 if (!$dialog) throw new Error('dialog element not found.');
-
-const $addButton = document.querySelector('.new-event');
+var $addButton = document.querySelector('.new-event');
 if (!$addButton) throw new Error('add new entry button not found.');
-$addButton.addEventListener('click', () => {
+$addButton.addEventListener('click', function () {
   $dialog.showModal();
 });
-
-const $cancelModal = document.querySelector('.cancel-modal');
+var $cancelModal = document.querySelector('.cancel-modal');
 if (!$cancelModal) throw new Error('cancel button not found within modal.');
-
-$cancelModal.addEventListener('click', () => {
+$cancelModal.addEventListener('click', function () {
   $dialog.close();
 });
-
-const $confirmModal = document.querySelector('.confirm-modal');
-
-$confirmModal.addEventListener('click', (event: Event) => {
+var $confirmModal = document.querySelector('.confirm-modal');
+$confirmModal.addEventListener('click', function (event) {
   event.preventDefault();
   console.log('inside the confirm Modal.');
 });
-
 // const $photoInput = document.querySelector('input[name="photo"]');
